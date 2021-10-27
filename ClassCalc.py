c9a891a03765e4cb77b33b5baf243f9f674c265a
Audrey Boni <59347694+audreybonee@users.noreply.github.com>
2021-10-27 16:23:12 -0400

    Class calculator

---
+++ ClassCalc.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  5 18:02:22 2021
+
+@author: audre
+"""
+
+class Calculator:
+    
+        
+        
+    def add(self, x, y):
+        self.__add = x +y
+        return self.__add
+    def subtract(self, x, y):
+        self.__subtract = x - y
+        return self.__subtract
+    def multiply(self, x, y):
+        self.__multiply = x * y
+        return self.__multiply
+        
+    def divide(self, x, y):
+        if y == 0:
+            print("You can't divide by zero!")
+        else:
+            self.__divide = x/y
+            return self.__divide
+        
+        
